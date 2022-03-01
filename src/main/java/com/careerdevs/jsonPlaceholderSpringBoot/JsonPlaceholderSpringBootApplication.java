271f070799f20d1ef401165e729fe08be30df52a
mguadagni <guadagnimichael@gmail.com>
2022-03-01 17:58:36 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/jsonPlaceholderSpringBoot/JsonPlaceholderSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonPlaceholderSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceholderSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPlaceholderSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/jsonPlaceholderSpringBoot/JsonPlaceholderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonPlaceholderSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceholderSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
